535b631b2e89f5838e6a4459a8054ece4f0d8e45
Joao Neves <jneves95@hotmail.com>
2021-03-18 12:41:36 +0000

    Import projects in local repo

---
+++ LeetCode/src/com/leetcode/CircleRandomPoint.java
@@ -0,0 +1,43 @@
+package com.leetcode;
+
+/*
+ * y^2 = r^2 - x^2
+ * -Math.sqrt(Math.pow(r, 2) - Math.pow(x, 2)) <= y <= Math.sqrt(Math.pow(r, 2) - Math.pow(x, 2))
+ */
+public class CircleRandomPoint {
+	
+	double RAD, X, Y;
+	
+	public CircleRandomPoint(double radius, double x_center, double y_center) {
+		RAD = radius;
+		X = x_center;
+		Y = y_center;
+	}
+		
+	public double[] randPoint() {
+		double ang = Math.random() * 2 * Math.PI;
+		double hyp = Math.random() * RAD;
+		double x = X + Math.cos(ang) * hyp;
+		double y = Y + Math.sin(ang) * hyp;
+		
+		return new double[] {x, y};
+	}
+	
+	public static void main(String[] args) {
+		CircleRandomPoint crp = new CircleRandomPoint(10, 5, -7.5);
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Test " + i + ":");
+			
+			double[] result = crp.randPoint();
+			
+			for (double d : result) {
+				System.out.println(d);
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ LeetCode/src/com/leetcode/trees/AddOneRow.java
@@ -0,0 +1,68 @@
+package com.leetcode.trees;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * LeetCode 9 March 2021 Challenge
+ * 
+ * Given a binary tree, adds a row of nodes with value V at depth D
+ */
+public class AddOneRow {
+	
+	public TreeNode addOneRow(TreeNode root, int v, int d) {
+		// Get nodes of depth D
+		List<TreeNode> nodes = new ArrayList<>();
+		
+		// If inserting at root level
+		if (d == 1) {
+			TreeNode newRoot = new TreeNode(v, root, null);
+			return newRoot;
+		}
+		
+		int depth = 1;
+		nodes.add(root);
+		while (depth < d - 1) {
+			List<TreeNode> nextRow = new ArrayList<>();
+			for (TreeNode n : nodes) {
+				if (n.left != null) nextRow.add(n.left);
+				if (n.right != null) nextRow.add(n.right);
+			}
+			depth++;
+			nodes = nextRow;
+		}
+		
+		// Insert new row of nodes
+		for (TreeNode n : nodes) {
+			TreeNode left = new TreeNode(v, n.left, null);
+			TreeNode right = new TreeNode(v, null, n.right);
+			n.left = left;
+			n.right = right;
+		}
+		
+		return root;
+	}
+
+	
+
+	public static void main(String[] args) {
+		AddOneRow aor = new AddOneRow();
+		TreeNode a = new TreeNode(4);
+		TreeNode b = new TreeNode(2);
+		TreeNode c = new TreeNode(6);
+		TreeNode d = new TreeNode(3);
+		TreeNode e = new TreeNode(1);
+		TreeNode f = new TreeNode(5);
+		TreeNode g = new TreeNode(8);
+		
+		a.left = b;
+		a.right = c;
+		b.left = d;
+		b.right = e;
+		c.left = f;
+		f.right = g;
+		
+		a.simplePrint();
+		aor.addOneRow(a, 9, 2);
+		a.simplePrint();
+	}
+}	
